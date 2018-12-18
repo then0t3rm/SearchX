7ec176dba3eba4dc13250d44b5321a21981700a1
then0t3rm <45977121+then0t3rm@users.noreply.github.com>
2018-12-18 16:03:36 -0200

    Create SearchX.py
    Using:
    
    python2 SearchX.py http://<url>/ common.txt

---
+++ SearchX.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python2
+#c0d3d by n0t3rm
+import os
+import requests
+import sys
+##################
+os.system("clear")
+##################
+url = sys.argv[1]
+arquivo = open(sys.argv[2])
+
+print """\t\t\n\033[34m\033[1m
+ ___                  _   __  __
+/ __| ___ __ _ _ _ __| |_ \ \/ /
+\__ \/ -_) _` | '_/ _| ' \ >  <
+|___/\___\__,_|_| \__|_||_/_/\_\
+
+\033[0;0m"""
+
+print "#"*80
+######################
+########
+
+for linhas in arquivo:
+    linhas = linhas.rstrip('\n')
+    link = url+ linhas+'/'
+    request = requests.get(link, timeout=15)
+    status = request.status_code
+    if status == 200:
+        print "[\033[32m\033[1m+\033[0;0m] %s ==> encontrado!"%(link)
+    elif status == 403:
+        print "[\033[33m\033[1m!\033[0;0m] %s ==> acesso negado!"%(link)
+####
